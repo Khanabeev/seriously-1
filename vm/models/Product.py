fe2631237d88ffa6afd3ea65af4031464e738f78
Anton_Khanabeev <anton.khanabeev@coupay.co.uk>
2022-02-23 06:19:42 +0300

    FEAT: add models and service directories

---
+++ vm/models/Product.py
@@ -0,0 +1,5 @@
+class Product:
+
+    def __init__(self, name: str, price: int):
+        self.price = price
+        self.name = name
---
+++ vm/services/VendingMachine.py
@@ -0,0 +1,16 @@
+from vm.models.Product import *
+
+
+class VendingMachine:
+
+    def __init__(self):
+        pass
+
+    def add_products(self, product: Product):
+        pass
+
+    def remove_product(self, product_id: int):
+        pass
+
+    def show_one_product(self, product_id: int):
+        pass
